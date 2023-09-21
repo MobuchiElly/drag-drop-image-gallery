fe1e363a48646360e643e1514ace36d8a19b8202
MobuchiElly <ugwueleazer@gmail.com>
2023-09-21 17:59:18 +0100

    fix

--- src/App_Components/firebase.js
+++ src/App_Components/firebase.js
@@ -6,14 +6,16 @@ import { getAuth } from "/node_modules/firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyATrEpAiX-UnNg4wTugDiPNJwqWMHzsnOg",
-  authDomain: "drag-drop-image-gallery.firebaseapp.com",
-  projectId: "drag-drop-image-gallery",
-  storageBucket: "drag-drop-image-gallery.appspot.com",
-  messagingSenderId: "303556606223",
-  appId: "1:303556606223:web:e12fdf8a373a6f0e9c267a"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
