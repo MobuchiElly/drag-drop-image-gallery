bb13894c15d70310beebcbd5ea66238f8e59897f
MobuchiElly <ugwueleazer@gmail.com>
2023-11-14 23:09:13 +0100

    implemented Footer UI

--- src/App_Components/App.js
+++ src/App_Components/App.js
@@ -3,6 +3,7 @@ import { auth } from './firebase';
 import Home from './Home';
 import Login from './Login';
 import Search from './Search';
+import Footer from './Footer';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -26,6 +27,7 @@ function App() {
         <div>
           <Search query={query} setQuery={setQuery} />
           {query ? null : <Home loggedIn={loggedIn}/>}
+          <Footer />
         </div>
       ) : (
         // User is not logged in, render the Login component
---
+++ src/App_Components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa';
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="container-fluid footer text-light shadow-lg mt-5">
+      <div className="row p-4">
+      <div className="col-sm-12 d-flex flex-column align-items-center">
+            
+            <div className="social-icons d-flex flex-row justify-content-center">
+              <a
+                href="https://www.linkedin.com/in/eleazer-ugwu-662211200/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FaLinkedinIn color="white" size={20}/>
+              </a>
+
+              <a
+                href="https://github.com/MobuchiElly"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub color="white" size={20}/>
+              </a>
+              
+              <a href="">
+                <FaTwitter color="white" size={20}/>
+              </a>
+            </div>
+
+          <div className="col-sm-12 d-flex justify-content-center">
+            <h3>&copy; BuchiDev</h3>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
